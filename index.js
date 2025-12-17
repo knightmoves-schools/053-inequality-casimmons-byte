6479ff5c67151fe5e590e2787909b7896c9e7d54
casimmons-byte <casimmons@anamosa.k12.ia.us>
2025-12-17 17:54:44 +0000

    if func

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-
+function guessNumber(number) {
+  if (number != 25) {
+    return document.getElementById('result').innerHTML = 'not it';
+  }else{
+    document.getElementById('result').innerHTML = 'it';
+  };
+};
+function strictGuessNumber(number) {
+  if (number !== 25) {
+    return document.getElementById('result').innerHTML = 'not it';
+  }else{
+    document.getElementById('result').innerHTML = 'it';
+  };
+};
